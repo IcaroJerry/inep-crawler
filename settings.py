9e2967890b863dc8726934dd63fe7a93fe92c31c
Icaro Jerry <icarojerry@potelo.com.br>
2019-08-05 00:13:14 -0300

    Corrige url

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 INEP_URL = "http://inep.gov.br/microdados"
 
-REPOSITORY_URL = "https://github.com/IcaroJerry/enade-analyser"
+REPOSITORY_URL = "https://github.com/IcaroJerry/inep-crawler"
 
 OPEN_ISSUE_URL = REPOSITORY_URL + "/issues/new"
